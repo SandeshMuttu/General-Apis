24f699ba3844232f02ead18bc02c2856c737eb8d
Sandesh <sandeshmuttubp96@gmail.com>
2022-11-19 23:48:36 +0530

    added new api helloWorld

--- src/main/java/com/example/general/dev/controller/GeneralApi.java
+++ src/main/java/com/example/general/dev/controller/GeneralApi.java
@@ -1,8 +1,6 @@
 package com.example.general.dev.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -11,5 +9,10 @@ public class GeneralApi {
     @GetMapping("/hello")
     public String hellWorld() {
         return "Hello Mate";
+    }
+
+    @GetMapping("/hello/member")
+    public String hellWorld(@RequestParam String name) {
+        return "Hello:" + name ;
     }
 }
